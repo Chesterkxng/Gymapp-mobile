74b3ee5b70b8782eb0c326d2538457a61db743b9
Chesterkxng <cgoita00@gmail.com>
2026-01-22 17:46:02 +0100

    assets: icons used and routing file

---
+++ assets/icons/index.ts
@@ -0,0 +1,51 @@
+import IDCard from './id-card.svg';
+import Password from './password-fill.svg';
+import LeftArrow from './arrow-left.svg';
+import Email from './email.svg';
+import Check from './check.svg';
+import Cross from './cross.svg';
+import Profile from './profile.svg';
+import Access from './qr.svg';
+import Progress from './progress.svg';
+import Home from './home.svg';
+import Scan from './scan.svg';
+import Male from './male.svg';
+import Female from './female.svg'
+import RightChevron from './chevron-right.svg';
+import AddressBook from './address-book.svg';
+import Heart from './heart.svg';
+import Card from './card.svg';
+import Payment from './payment.svg';
+import Save from './save.svg';
+import Logout from './logout.svg';
+import Calendar from './calendar.svg';
+import Notification from './notification.svg';
+import Shop from './shop.svg';
+import Birthdate from './birthday.svg';
+
+export const Icons = {
+    idCard: IDCard,
+    password: Password,
+    leftArrow: LeftArrow,
+    email: Email,
+    check: Check,
+    cross: Cross,
+    profile: Profile,
+    access: Access,
+    progress: Progress,
+    home: Home,
+    scan: Scan,
+    male: Male,
+    female: Female,
+    rightChevron: RightChevron,
+    addressBook: AddressBook,
+    heart: Heart,
+    card: Card,
+    payment: Payment,
+    save: Save,
+    logout: Logout,
+    calendar: Calendar,
+    notification: Notification,
+    shop: Shop,
+    birthdate: Birthdate
+}
